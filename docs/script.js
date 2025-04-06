94a79ad80ea2b31b3ba7cee8381c2b5791c7a31a
mschnelk <mia.schnelker@pitt.edu>
2025-04-05 20:55:41 -0400

    big big changes
    i added a filter & did some styling on the info page & just cleaned it up overall. very happy love her. for the future (aka next unit) - fix the filter so that the images are aligned correctly even when filtered

--- docs/script.js
+++ docs/script.js
@@ -1,45 +1,62 @@
-let allImages = [];
-let loadedImages = 0;
-const imagesPerLoad = 12;
+function getFilterFromURL() {
+    const params = new URLSearchParams(window.location.search);
+    return params.get('filter') || 'all';
+  }
+  
+const initialFilter = getFilterFromURL();
+filterSelection(initialFilter);
 
-async function fetchImages() {
-    const response = await fetch('/get-images');
-    allImages = await response.json();
-    loadedImages = 0;
-    document.getElementById('flash-1').innerHTML = '';
-    document.getElementById('flash-2').innerHTML = '';
-    document.getElementById('gallery-3').innerHTML = '';
-    document.getElementById('gallery-4').innerHTML = '';
-    loadMoreImages();
+function filterSelection(c) {
+  var y = document.getElementsByClassName("gallery-column");
+  var x = [];
+  for (var i = 0; i < y.length; i++)
+  {
+    x = x.concat(Array.from(y[i].getElementsByTagName('img')));
+  }
+
+  if (c == "all") c = "";
+  // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
+  for (i = 0; i < x.length; i++) {
+    removeClass(x[i], "show");
+    if (x[i].className.indexOf(c) > -1) addClass(x[i], "show");
+  }
 }
 
-function loadMoreImages() {
-    const galleries = [
-        document.getElementById('flash-1'),
-        document.getElementById('flash-2'),
-        document.getElementById('flash-3'),
-        document.getElementById('flash-4')
-    ];
-
-    const remainingImages = allImages.length - loadedImages;
-    const imagesToLoad = Math.min(imagesPerLoad, remainingImages);
-
-    for (let i = 0; i < imagesToLoad; i++) {
-        const image = allImages[loadedImages + i];
-        const imgElement = document.createElement('img');
-        imgElement.src = `images/${image}`;
-        imgElement.alt = image;
-        galleries[(loadedImages + i) % 4].appendChild(imgElement);
+// Show filtered elements
+function addClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    if (arr1.indexOf(arr2[i]) == -1) {
+      element.className += " " + arr2[i];
     }
-
-    loadedImages += imagesToLoad;
-
-    // Hide the button if all images are loaded
-    if (loadedImages >= allImages.length) {
-        document.getElementById('load-more').style.display = 'none';
-    }
+  }
 }
 
-document.getElementById('load-more').addEventListener('click', loadMoreImages);
+// Hide elements that are not selected
+function removeClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    while (arr1.indexOf(arr2[i]) > -1) {
+      arr1.splice(arr1.indexOf(arr2[i]), 1);
+    }
+  }
+  element.className = arr1.join(" ");
+}
 
-fetchImages();
+const btns = document.getElementById("filter").getElementsByTagName("button");
+for (let i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+    const current = document.getElementsByClassName("current");
+    if (current[0]) current[0].classList.remove("current");
+    this.classList.add("current");
+  });
+
+  // Set the initial "current" button
+  if (btns[i].getAttribute("data-filter") === initialFilter) {
+    btns[i].classList.add("current");
+  }
+}
